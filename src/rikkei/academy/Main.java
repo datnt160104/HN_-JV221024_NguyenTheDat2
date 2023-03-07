38a73c0ddc17af5468f9438006a232dc31d30f7e
Đạt <dat16010425@gmail.com>
2023-03-07 16:05:01 +0700

    test2

---
+++ src/rikkei/academy/Main.java
@@ -0,0 +1,9 @@
+package rikkei.academy;
+
+public class Main {
+    public static void main(String[] args) {
+        Class_Student student = new Class_Student();
+        student.inputData();
+        student.displayData();
+    }
+}
---
+++ src/rikkei/academy/Student.java
@@ -0,0 +1,4 @@
+package rikkei.academy;
+
+public class Student extends Class_Student{
+}
